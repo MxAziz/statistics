9c0bfb93c7091bdb49b30bf3fee253bdaf418197
MxAziz <hereabdulaziz@gmail.com>
2026-09-21 22:55:07 +0600

    update chi-square

--- code/chi-square.r
+++ code/chi-square.r
@@ -7,14 +7,14 @@ chisq_data <- rchisq(n, df)
 
 cat("Sample Mean       :", mean(chisq_data), " (Theoretical:", df, ")\n")
 cat("Sample Variance    :", var(chisq_data), " (Theoretical:", 2*df, ")\n")
-cat("Sample Skewness    :", mean((chisq_data-mean(chisq_data))^3)/sd(chisq_data)^3,
-    " (Theoretical:", sqrt(8/df), ")\n")
+# cat("Sample Skewness    :", mean((chisq_data-mean(chisq_data))^3)/sd(chisq_data)^3,
+    # " (Theoretical:", sqrt(8/df), ")\n")
 
 hist(chisq_data, breaks = 50, probability = TRUE, col = "lightblue",
      main = paste("Chi-square Distribution (df =", df, ")"), xlab = "x")
 curve(dchisq(x, df), add = TRUE, col = "red", lwd = 2)
 
-# Relationship: sum of squares of standard normal variables
+# Relationship: sum of squares of standard normal variables -- chaile bad diye dio.
 z <- matrix(rnorm(n*df), ncol = df)
 chisq_manual <- rowSums(z^2)
 cat("\nMean of manually generated Chi-square:", mean(chisq_manual), "\n")
